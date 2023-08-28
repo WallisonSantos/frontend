365107bb6e3a0992ddda54981a35b15fc2e5dae3
WallisonSantos <wallisontiagoassis206@outlook.com>
2023-08-28 08:16:50 -0300

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Editar o <code>src/App.tsx</code> e salve para carregar.
         </p>
         <a
           className="App-link"
@@ -16,13 +16,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          About
+          Learn React
         </a>
       </header>
     </div>
   );
 }
-
-
 
 export default App;
